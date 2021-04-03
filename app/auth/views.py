f4645ba1cfc5f951a07c7b995680cd92fc12cd67
Scarcrux <reg@scarcrux.com>
2021-04-02 22:18:15 -0400

    Add Login and Unconfirmed Templates - #9

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,47 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_required, current_user
 from ..email import send_email
 from . import auth
-from .. import db
+from app import db
 from models.user import User
-from .forms import RegistrationForm
+from .forms import LoginForm, RegistrationForm
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
+                and request.endpoint \
+                and request.blueprint != 'auth' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+@auth.route(‘/login’, methods=[‘GET’, ‘POST’])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data.lower())
+    if user is not None and user.verify_password(form.password.data):
+         login_user(user, form.remember_me.data)
+         next = request.args.get('next')
+         if next is None or not next.startswith('/'):
+             next = url_for('main.index')
+         return redirect(next)
+    flash('Invalid email or password.')
+    return render_template('auth/login.html', form=form)
+
+@auth.route(‘/logout’)
+@login_required
+def logout():
+	logout_user()
+	flash(‘You have been logged out.’)
+	return redirect(url_for(‘main.index’)
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
@@ -36,7 +73,8 @@ def confirm(token):
 @auth.route('/confirm')
 @login_required
 def resend_confirmation():
-   	token = current_user.generate_confirmation_token()
-   	send_email(current_user.email, 'Confirm Your Account', 'auth/email/confirm', user=current_user, token=token)
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+        'auth/email/confirm', user=current_user, token=token)
     flash('A new confirmation email has been sent to you by email.')
     return redirect(url_for('main.index'))
