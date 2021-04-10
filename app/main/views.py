619e9f54c99deb2d2bc731b24a30e5b27f69f8c2
Scarcrux <reg@scarcrux.com>
2021-04-09 21:49:21 -0400

    Add Edit Profile Forms - #24

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,14 @@
 from flask import render_template
 
 from . import main
+from models.user import User
 
 # Index
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
