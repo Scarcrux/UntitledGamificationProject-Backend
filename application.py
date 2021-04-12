c3407efee1a2c0250f89cdf0666188baceeb7dd0
Scarcrux <reg@scarcrux.com>
2021-04-11 20:16:30 -0400

    Update Unit Tests - #25

--- application.py
+++ application.py
@@ -21,3 +21,9 @@ def deploy():
     # create or update user roles
     Role.insert_roles()
 
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
--- tests/test_followers.py
+++ tests/test_followers.py
@@ -5,7 +5,7 @@ from app import create_app, db
 from models.user import User
 from models.post import Post
 
-class UserModelCase(unittest.TestCase):
+class FollowerModelCase(unittest.TestCase):
     def setUp(self):
         app = create_app('testing')
         ctx = app.app_context()
@@ -15,18 +15,6 @@ class UserModelCase(unittest.TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
-
-    def test_password_hashing(self):
-        u = User(username='susan')
-        u.set_password('cat')
-        self.assertFalse(u.verify_password('dog'))
-        self.assertTrue(u.verify_password('cat'))
-
-    def test_avatar(self):
-        u = User(username='john', email='john@example.com')
-        self.assertEqual(u.gravatar(128), ('https://secure.gravatar.com/avatar/'
-                                         'd4c74594d841139328695756648b6bd6'
-                                         '?s=128&d=identicon&r=g'))
 
     def test_follow(self):
         u1 = User(username='john', email='john@example.com')
