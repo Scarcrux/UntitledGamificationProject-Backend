c3d4cb3f5f13285b60da4adfcc536a6ac60830f9
Scarcrux <reg@scarcrux.com>
2021-04-24 23:47:09 -0400

    Update Schemas - #34

--- resources/store.py
+++ resources/store.py
@@ -1,20 +1,28 @@
 from flask_restful import Resource
 from models.store import StoreModel
+from schemas.store import StoreSchema
 
+NAME_ALREADY_EXISTS = "A store with name '{}' already exists."
+ERROR_INSERTING = "An error occurred while inserting the store."
+STORE_NOT_FOUND = "Store not found."
+STORE_DELETED = "Store deleted."
+
+store_schema = StoreSchema()
+store_list_schema = StoreSchema(many=True)
 
 class Store(Resource):
     @classmethod
     def get(cls, name):
         store = StoreModel.find_by_name(name)
         if store:
-            return store.json()
-        return {"message": "Store not found."}, 404
+            return store_schema.dump(store), 200
+        return {"message": STORE_NOT_FOUND}, 404
 
     @classmethod
     def post(cls, name):
         if StoreModel.find_by_name(name):
             return (
-                {"message": "A store with name '{}' already exists.".format(name)},
+                {"message": NAME_ALREADY_EXISTS.format(name)},
                 400,
             )
 
@@ -22,20 +30,22 @@ class Store(Resource):
         try:
             store.save_to_db()
         except:
-            return {"message": "An error occurred while creating the store."}, 500
+            return {"message": ERROR_INSERTING}, 500
 
-        return store.json(), 201
+        return store_schema.dump(store), 201
 
     @classmethod
     def delete(cls, name):
         store = StoreModel.find_by_name(name)
         if store:
             store.delete_from_db()
+            return {"message": STORE_DELETED}, 200
 
-        return {"message": "Store deleted."}
+        return {"message": STORE_NOT_FOUND}, 404
 
 
 class StoreList(Resource):
     @classmethod
     def get(cls):
-        return {"stores": [x.json() for x in StoreModel.find_all()]}
+        return {"stores": store_list_schema.dump(StoreModel.find_all())}, 200
+
